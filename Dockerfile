b1c58aeb35311c8ccecb0c530fdc12d101791036
kanelouise <kane@kanes-Air.lan>
2025-05-23 00:02:24 -0700

    meh: turn off master key for deployment

--- Dockerfile
+++ Dockerfile
@@ -33,6 +33,8 @@ COPY . .
 # Precompile bootsnap code for faster boot times
 RUN bundle exec bootsnap precompile app/ lib/
 
+#defining before the precompile
+ENV SECRET_KEY_BASE=DUMMY
 # Precompiling assets for production without requiring secret RAILS_MASTER_KEY
 RUN SECRET_KEY_BASE_DUMMY=1 ./bin/rails assets:precompile
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,9 +16,9 @@ Rails.application.configure do
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
-  # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
+  # Ensures that a master key IS NOT REQUIRED in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = false
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   # config.public_file_server.enabled = false
